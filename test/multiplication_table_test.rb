2981511f5b7cee44f728f57bb3e67b9440ac0d25
Arthur Granado <agranado@pluga.co>
2017-08-19 14:13:38 -0300

    Create Multiplication Table for N primes

--- test/multiplication_table_test.rb
+++ test/multiplication_table_test.rb
@@ -1,6 +1,21 @@
 require "test_helper"
 
 class MultTable
+  def create_table(limit=10)
+    primes = prime_numbers(limit)
+    table = [["#"] + primes]
+    primes.each{|prime| table.push(create_row(prime, primes))}
+    table
+  end
+
+  def create_row(current, numbers)
+    [current]+mult_in_row(current, numbers)
+  end
+
+  def mult_in_row(current, numbers)
+    numbers.reduce([]){|r, other| r.push(current*other)}
+  end
+
   def prime_numbers(limit=10, my_prime=MyPrime.new)
     primes = []
     candidate = 2
@@ -21,5 +36,16 @@ class MultTableTest < Minitest::Test
     prime_numbers = [2,3,5,7,11,13,17,19,23,29]
 
     assert_equal prime_numbers, @mult_table.prime_numbers
+  end
+
+  def test_multi_table_for_3_first_prime_numbers
+    table = [
+      ["#",2,3,5],
+      [2,4, 6,10],
+      [3,6,9,15],
+      [5,10,15,25]
+    ]
+
+    assert_equal table, @mult_table.create_table(3)
   end
 end
