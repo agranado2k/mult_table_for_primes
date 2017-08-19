e96ba90d4917d92a7b984e073f86eb14b5a26c68
Arthur Granado <agranado@pluga.co>
2017-08-19 11:01:49 -0300

    Init repo with prime test

---
+++ test/check_if_number_is_prime_test.rb
@@ -0,0 +1,31 @@
+require "minitest/autorun"
+
+class IsPrimeTest < Minitest::Test
+  def setup
+    @my_prime = MyPrime.new
+  end
+
+  def test_number_less_than_2_cannot_be_prime
+    refute @my_prime.prime?(1)
+  end
+
+  def test_2_is_prime
+    assert @my_prime.prime?(2)
+  end
+
+  def test_3_is_prime
+    assert @my_prime.prime?(3)
+  end
+
+  def test_5_is_prime
+    assert @my_prime.prime?(5)
+  end
+
+  def test_4_is_not_prime
+    assert @my_prime.prime?(4)
+  end
+
+  def test_10_is_not_prime
+    assert @my_prime.prime?(10)
+  end
+end
